a59c4bd44ee08982547d5f3414506237ced3159c
pranto113015 <pranto3876@gmail.com>
2024-06-18 12:31:17 +0600

    added home section

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,13 +19,20 @@ if (navClose) {
 /*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll('.nav__link');
 
-const linkAction = () =>{
+const linkAction = () => {
     const navMenu = document.getElementById('nav-menu')
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
-/*=============== SHADOW HEADER ===============*/
 
+
+/*=============== SHADOW HEADER ===============*/
+const shadowHeader = () => {
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('shadow-header')
+        : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader)
 
 /*=============== SWIPER FAVORITES ===============*/
 
